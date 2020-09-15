66a92487a1563b6a8a2978d2376e885a289d4e8a
srivastava.hina@gmail.com <srivastava.hina@gmail.com>
2020-09-15 08:50:31 +0530

    Step3.3

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContext options) : base()
-        { 
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
         
         }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
