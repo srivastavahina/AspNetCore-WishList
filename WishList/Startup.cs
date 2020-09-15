ace431d696af627d6c7fa95ee211556ec38f401f
srivastava.hina@gmail.com <srivastava.hina@gmail.com>
2020-09-15 08:52:07 +0530

    Step3.3

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,7 +19,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
